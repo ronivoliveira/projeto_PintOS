712b4e59ec9098e654945c13616512f3e8e11a02
ronivoliveira <107883164+ronivoliveira@users.noreply.github.com>
2026-05-14 17:07:36 -0300

    Add files via upload

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -30,11 +30,15 @@ static void busy_wait (int64_t loops);
 static void real_time_sleep (int64_t num, int32_t denom);
 static void real_time_delay (int64_t num, int32_t denom);
 
+static struct list lista_do_soninho; //LISTA DAS THREADS EM ESTADO DE SONO
+
+
 /* Sets up the timer to interrupt TIMER_FREQ times per second,
    and registers the corresponding interrupt. */
 void
 timer_init (void) 
 {
+  list_init(&lista_do_soninho); //INICIALIZA LISTA
   pit_configure_channel (0, 2, TIMER_FREQ);
   intr_register_ext (0x20, timer_interrupt, "8254 Timer");
 }
